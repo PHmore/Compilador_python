2e3d8cf6d8452822045305d74795a6de0bfce397
PHmore <patryckhenryck401@gmail.com>
2024-03-15 18:39:02 -0300

    planejamento

---
+++ lexer/rules.py
@@ -0,0 +1,10 @@
+#obs: devemos usar um autômato deterministico para diminuir o gasto computacional   
+
+# Devemos primeiramente estudar as regras de sintaxé do python e classifica-las bem como criar o regex correspondente para cada uma
+
+# Rules:
+
+# Não podemos começar um indentificador com número ou caracter não alfabetico;
+
+# Não podemos usar palavras reservadas
+
